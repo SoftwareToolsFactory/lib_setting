21ce9e36c28d513adea7576e914bd5be6f5cdc4f
Nuroferatu <32645518+Nuroferatu@users.noreply.github.com>
2019-11-30 01:17:23 +0100

    make will search for cpp files in src/[platform] - for platform dependent source

--- buildcommon.mk
+++ buildcommon.mk
@@ -11,17 +11,20 @@
 ## |                   http://softwaretoolsfactory.com                       |
 ## '-------------------------------------------------------------------------'
 ## ----= Change log =---------------------------------------------------------
+##     3. 2019.11.30, 01:15 Nuroferatu   [+] [SCL-29] Multiplatform build
 ##     2. 2019.11.30, 00:40 Nuroferatu   [+] [SCL-36] OS_NAME variable, that stores system name
 ##     1. 2019.11.11, 13:16 Nuroferatu   [+] Initial
 ##                                           * Definitions of vpath based of prjcfg VARIABLES
 ##                                           * Terminal colors for echo and echo as ${LOG}
 ## ---------------------------------------------------------------------------
 ## TODO: This should go to some global path, and be used as part of buld chain of STF applications
+
 OS_NAME := $(shell uname -s | tr A-Z a-z)
 ## On FreeBSD Unix we need to rename OS_NAME
 ifeq (${OS_NAME},freebsd)
   OS_NAME=unix
 endif
+
 include prjcfg.mk
 
 ## Sample usage:
@@ -39,7 +42,7 @@ CMAGNETA := "\e[35m"
 CCYAN := "\e[36m"
 CWHITE := "\e[37m"
 
-vpath %.cpp ${SRC_DIR}
+vpath %.cpp ${SRC_DIR} ${SRC_DIR}/${OS_NAME}
 vpath %.h   ${INC_DIR}
 vpath %.d   ${TMP_DIR}
 vpath %.o   ${TMP_DIR}
